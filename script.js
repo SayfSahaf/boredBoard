02aad989ce56e9f672505507cacaeb6f47c9cc20
Sayf Sahaf <sayf.sahaf@gmail.com>
2023-09-08 10:20:50 -0400

    add buttons, color picker, slider

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-let boxCount = 4;
+let boxCount = 16;
 let actlCount = boxCount*boxCount;
-let boxSize = Math.floor(608/boxCount);
+let boxSize = Math.round(608/boxCount);
 
 const board = document.getElementById("board");
 board.style.gridTemplateColumns = `repeat(${boxCount}, ${boxSize}px)`;
-
-
-
 
 
 for (let i = 0; i < actlCount; i++) {
