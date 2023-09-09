b295a0929017639264315cef77659d85da60f855
Sayf Sahaf <sayf.sahaf@gmail.com>
2023-09-08 21:41:19 -0400

    implement working slider and board size change

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-let boxCount = 16;
-let actlCount = boxCount*boxCount;
-let boxSize = Math.round(608/boxCount);
-
+const slider = document.getElementById('slider')
 const board = document.getElementById("board");
-board.style.gridTemplateColumns = `repeat(${boxCount}, ${boxSize}px)`;
+const sValue = document.getElementById('sliderValue');
 
 
-for (let i = 0; i < actlCount; i++) {
-    const box = document.createElement("div")
-    box.style.height = `${boxSize}px`
-    box.style.width = `${boxSize}px`
-    box.classList.add("box")
-    board.appendChild(box)
+
+function drawBoard() {
+    let boxCount = slider.value;
+    let actlCount = boxCount*boxCount;
+    let boxSize = Math.round(608/boxCount);
+    sValue.innerText = slider.value + ' x ' + slider.value ;
+    board.innerHTML = ''
+    board.style.gridTemplateColumns = `repeat(${boxCount}, ${boxSize}px)`;
+
+    for (let i = 0; i < actlCount; i++) {
+        const box = document.createElement("div")
+        box.style.height = `${boxSize}px`
+        box.style.width = `${boxSize}px`
+        box.classList.add("box")
+        board.appendChild(box)
+    }
+
 }
+
+//initial board when website is first loaded up
+drawBoard();
+
+//new board is generated everytime the slider is changed
+slider.addEventListener('input', drawBoard)
+
+
+
+
+
 
 
 
